c49649c42947e5ad3eb352c900f869492e788a20
Raghav <raghavagarwal3050@gmail.com>
2022-12-07 22:44:36 +0530

    Removed unnecessary comments and formatting

--- stereo_mono_pre_post.py
+++ stereo_mono_pre_post.py
@@ -1,38 +1,3 @@
-#!/usr/bin/env python3
-
-
-
-# def get_frame_on_computer(frame1):
-#     frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
-    
-#     input_batch = transform(frame1).to(device2)
-#     print(np.asarray(input_batch).shape)
-#     with torch.no_grad():
-#         prediction = midas(input_batch)
-#         #print(prediction.shape)
-#         # print(frame.shape[:2])
-#         prediction = torch.nn.functional.interpolate(
-#             prediction.unsqueeze(1),
-#             size=frame1.shape[:2],
-#             mode="bicubic",
-#             align_corners=False,
-#         ).squeeze()
-#     #print(prediction.shape)
-#     frame1 = prediction.cpu().numpy()
-#     #print(frame)
-#     # frame1=cv2.normalize(frame1,None,0,1,norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_64F)
-#     # frame1=(frame1*255).astype(np.uint8)
-#     # frame1=cv2.applyColorMap(frame1,cv2.COLORMAP_MAGMA)
-#     return frame1
-# def save_array(filename,Array):
-#     file = open(filename, "w")
-#     content = str(Array)
-#     file.write(content)
-#     file.close()
-#blob path of midas dataset 
-# prev_frame_time,new_frame_time,frame_count=0,0,0
-
-
 
 import cv2
 import depthai as dai
@@ -133,15 +98,12 @@ manipRgb.setNumFramesPool(60)
 manipStereo.initialConfig.setResize(*(shape[1:]))
 manipLeft.initialConfig.setResize(288,300)
 manipRight.initialConfig.setResize(288,300)
-#manipLeft.initialConfig.setFrameType(dai.ImgFrame.Type.BGR888p)
-#manipLeft.setNumFramesPool(60)
 
 
 manipLeft2.initialConfig.setResize(640, 400)
 manipRight2.initialConfig.setResize(640, 400)
 
-#manipRight.initialConfig.setFrameType(dai.ImgFrame.Type.BGR888p)
-#manipRight.setNumFramesPool(60)
+
 
 
 camRgb.video.link(manipRgb.inputImage)
@@ -170,8 +132,7 @@ nn_xout.input.setQueueSize(1000)
 
 Stereo_xout = pipeline.create(dai.node.XLinkOut)
 Stereo_xout.setStreamName("Stereo")
-# Stereo_xout.input.setBlocking(True)
-# Stereo_xout.input.setQueueSize(4)
+
 
 #linking -->
 monoLeft.out.link(manipLeft.inputImage)
@@ -186,28 +147,10 @@ manipLeft2.out.link(depth.left)
 manipRight2.out.link(depth.right)
 
 
-
-
-
-
-
 nn.out.link(nn_xout.input)
-
-
-
-#depth.disparity.link(Stereo_xout.input)
-
-
 depth.disparity.link(manipStereo.inputImage)
-
 manipStereo.out.link(Stereo_xout.input)
 
-
-# xoutVideo = pipeline.create(dai.node.XLinkOut)
-
-# xoutVideo.setStreamName("video")
-
-# Connect to device and start pipeline
 
 
 
@@ -217,61 +160,31 @@ def get_frame(imfFrame, shape):
 
 counter = 0
 with dai.Device(pipeline) as device:
-    device.setIrLaserDotProjectorBrightness(100) # in mA, 0..1200
+    device.setIrLaserDotProjectorBrightness(100) 
     device.setIrFloodLightBrightness(0) 
     startTime = time.monotonic()
     video = device.getOutputQueue(name="RGB_MIDAS_VIDEO", maxSize=1000, blocking=False)
     q = device.getOutputQueue(name="Stereo", maxSize=10, blocking=False)
     while True:
-        # frame_count+=1
+        
         videoIn = video.tryGet()
         if videoIn!=None:
             frame=get_frame(videoIn,shape)
-            #print(frame)
-            
-            #print('USB speed:',device.getUsbSpeed())
+    
             frame=cv2.normalize(np.float32(frame),None,0,1,norm_type=cv2.NORM_MINMAX)
-            #print(frame)
+           
             frame=((frame*255).astype(np.uint8))
             
-            #print(frame)
-            #print(frame.shape)
+           
             frame=cv2.applyColorMap(frame,cv2.COLORMAP_MAGMA)
-            # frame2=cv2.normalize(frame2,None,0,1,norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_64F)
-            
-            # frame2=((frame2*255).astype(np.uint8))
-        
-            #print(frame)
-            #print(frame.shape)
-            #frame2=cv2.applyColorMap(frame2,cv2.COLORMAP_MAGMA)
-            #frame=cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-            
-            #frame=cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-            #print(frame)
-            
+ 
             cv2.imshow('MIDAS-RGB-OAKD',frame)
-            counter+=1
-        #cv2.imshow('MIDAS-RGB-COMPUTER',frame2)
-        #cv2.imshow('Difference',frame2-frame)
-     
-        #print(avg_difference)
-        # Get BGR frame from NV12 encoded video frame to show with opencv
-        # Visualizing the frame on slower hosts might have overhead
-        
-        # if (frame_count%20==0):
-        #     new_frame_time = time.time()
-        #     fps = 1 / ((new_frame_time - prev_frame_time)/20)
-        #     prev_frame_time = new_frame_time
-        #     fps = str(int(fps))
-        
-        inDisparity = q.get()  # blocking call, will wait until a new data has arrived
+      
+        inDisparity = q.get()  
         frame2 = inDisparity.getFrame()
-        # Normalization for better visualization
+     
         frame2 = (frame2 * (255 / depth.initialConfig.getMaxDisparity())).astype(np.uint8)
 
-        # cv2.imshow("disparity", frame2)
-
-        # Available color maps: https://docs.opencv.org/3.4/d3/d50/group__imgproc__colormap.html
         frame2 = cv2.applyColorMap(frame2, cv2.COLORMAP_MAGMA)
         cv2.imshow("disparity_color", frame2)
 
--- stereo_pre_post.py
+++ stereo_pre_post.py
@@ -29,16 +29,13 @@ manipRight = pipeline.create(dai.node.ImageManip)
 manipLeft2 = pipeline.create(dai.node.ImageManip)
 manipRight2 = pipeline.create(dai.node.ImageManip)
 depth = pipeline.create(dai.node.StereoDepth)
-# image_manip_script = pipeline.create(dai.node.Script)
+
 depth.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
 # Options: MEDIAN_OFF, KERNEL_3x3, KERNEL_5x5, KERNEL_7x7 (default)
 depth.initialConfig.setMedianFilter(dai.MedianFilter.KERNEL_7x7)
 depth.setLeftRightCheck(lr_check)
 depth.setExtendedDisparity(extended_disparity)
 depth.setSubpixel(subpixel)
-
-
-
 
 
 
@@ -71,7 +68,8 @@ manipRight.initialConfig.setResize(288, 300)
 
 manipLeft2.initialConfig.setResize(640, 400)
 manipRight2.initialConfig.setResize(640, 400)
-# NN that detects faces in the image
+
+
 nnL = pipeline.create(dai.node.NeuralNetwork)
 nnL.setBlobPath("/home/raghav/Blur.blob")
 manipLeft.out.link(nnL.input)
@@ -81,84 +79,38 @@ nnR.setBlobPath("/home/raghav/Blur.blob")
 manipRight.out.link(nnR.input)
 
 
-# nn.out.link(image_manip_script.inputs['LEFT_IMAGE'])
-# nn.out.link(image_manip_script.inputs['RIGHT_IMAGE'])
 
-
-# nnR.passthrough.link(depth.right)
-# nnL.passthrough.link(depth.left)
-
-# xoutnnl = pipeline.create(dai.node.XLinkOut)
-# xoutnnl.setStreamName("nnl")
-# nnL.passthrough.link(xoutnnl.input)
-
-# xoutnnr = pipeline.create(dai.node.XLinkOut)
-# xoutnnr.setStreamName("nnr")
-# nnR.passthrough.link(xoutnnr.input)
 nnL.passthrough.link(manipLeft2.inputImage)
 nnR.passthrough.link(manipRight2.inputImage)
 manipLeft2.out.link(depth.left)
 manipRight2.out.link(depth.right)
 
-# print("sj")
+
 
 
 xout = pipeline.create(dai.node.XLinkOut) 
 xout.setStreamName("disparity")
 
-# Create a node that will produce the depth map (using disparity output as it's easier to visualize depth this way)
-# depth.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
-# Options: MEDIAN_OFF, KERNEL_3x3, KERNEL_5x5, KERNEL_7x7 (default)
-# depth.initialConfig.setMedianFilter(dai.MedianFilter.KERNEL_7x7)
-# depth.setLeftRightCheck(lr_check)
-# depth.setExtendedDisparity(extended_disparity)
-# depth.setSubpixel(subpixel)
 depth.disparity.link(xout.input)
-# image_manip_script.setScript("""
-    
-#     while True:
 
-#         output_L = node.io['LEFT_IMAGE'].get()
-#         output_R = node.io['RIGHT_IMAGE'].get()
-
-#         node.io['LEFT_FINAL'].send(output_L)
-#         node.io['RIGHT_FINAL'].send(output_R)
-
-    
-
-# """)
-# nn_xoutR = pipeline.create(dai.node.XLinkOut)
-# nn_xoutR.setStreamName("RIGHT_FRAME")
-
-# nn_xoutL = pipeline.create(dai.node.XLinkOut)
-# nn_xoutL.setStreamName("LEFT_FRAME")
 
 with dai.Device(pipeline) as device:
     
-    # nlq = device.getOutputQueue(name="nnl", maxSize=10, blocking=False)
-    # nlr = device.getOutputQueue(name="nnr", maxSize=10, blocking=False)
+
 
     q = device.getOutputQueue(name="disparity", maxSize=10, blocking=False)
 
-    # shape = (1, 288, 288)
-    # def get_frame(imfFrame, shape):
-    #     return np.array(imfFrame.get()).view(np.float16).reshape(shape).transpose(1, 2, 0).astype(np.uint8)
 
     while True:
-        # print(q)
-        # nlqf = getFrame(nlq)
-        # cv2.imshow("ei", nlqf)
-        # cv2.waitKey(1)
+     
         frame = getFrame(q)
        
-        # frame = cv2.resize(frame,shape)
-        # frame = inDisparity.getFrame()
-        # Normalization for better visualization
+     
         frame = (frame * (255 / depth.initialConfig.getMaxDisparity())).astype(np.uint8)
         
         cv2.imshow("disparity", frame)
         
-        # Available color maps: https://docs.opencv.org/3.4/d3/d50/group__imgproc__colormap.html
+       
         frame = cv2.applyColorMap(frame, cv2.COLORMAP_MAGMA)
         cv2.imshow("disparity_color", frame)
 
